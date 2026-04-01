2a60d3339bb9406c0ebb64446ed89f688ca62932
imamsalji <imamsalji7@gmail.com>
2026-04-01 22:34:02 +0700

    unit test needed

--- app/Http/Controllers/Api/SocialAuthController.php
+++ app/Http/Controllers/Api/SocialAuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 
 class SocialAuthController extends Controller
 {
